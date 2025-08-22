2497649db538cedac3c63301ff5c5b4cac35a544
Nithya Subramanian <98416062+nithyatsu@users.noreply.github.com>
2025-08-22 10:47:42 -0700

    Refactor Kubernetes persistent volume configuration

--- volume/main.tf
+++ volume/main.tf
@@ -1,26 +1,42 @@
-provider "kubernetes" {
-  config_path = var.kubeconfig
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
+
+variable "context" {
+  description = "Radius-provided object containing information about the resource calling the Recipe."
+  type = any
+}
+
+locals {
+  uniqueName = var.context.resource.name
+  namespace = var.context.runtime.kubernetes.namespace
 }
 
 resource "kubernetes_persistent_volume" "pv" {
   metadata {
-    name = var.name
+    name = var.context.resource.name
     labels = {
       app      = "redis"
-      resource = var.name
-      "radapp.io/application" = var.application != "" ? var.application : "someting"
+      resource = var.context.resource.name
+      # Label pods with the application name so `rad run` can find the logs.
+      "radapp.io/application" = var.context.application != null ? var.context.application.name : ""
     }
   }
 
   spec {
-    storage_class_name = var.storage_class
-
+    storage_class_name = "manual"
+    
     capacity = {
-      storage = var.size
+      storage = var.context.resource.properties.size
     }
-
-    access_modes = [var.access_mode]
-
+    
+    access_modes = ["ReadWriteOnce"]
+    
     persistent_volume_source {
       host_path {
         path = var.host_path
@@ -32,11 +48,16 @@ resource "kubernetes_persistent_volume" "pv" {
 
 output "result" {
   value = {
-    kind         = "persistent"
-    storageClass = kubernetes_persistent_volume.pv.spec[0].storage_class_name
-    capacity     = kubernetes_persistent_volume.pv.spec[0].capacity["storage"]
-    accessModes  = kubernetes_persistent_volume.pv.spec[0].access_modes
-    hostPath     = kubernetes_persistent_volume.pv.spec[0].persistent_volume_source[0].host_path[0].path
-    name         = kubernetes_persistent_volume.pv.metadata[0].name
+    resources = [
+      "/planes/kubernetes/local/providers/core/PersistentVolume/${var.context.resource.name}"
+    ]
+    values = {
+      kind         = "persistent"
+      storageClass = var.storage_class
+      capacity     = var.context.resource.properties.size
+      accessModes  = [var.context.resource.properties.accessMode == null ? var.access_mode : var.context.resource.properties.accessMode]
+      hostPath     = var.host_path
+      name         = var.context.resource.name
+    }
   }
 }
